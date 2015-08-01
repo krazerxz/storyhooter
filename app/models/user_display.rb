44ff235655383df40ecd9470c8a2dac00e048870
Chris Pomfret <chris.pomfret@bt.com>
2015-08-01 12:06:14 +0100

    referrer_name on user_display

--- app/models/user_display.rb
+++ app/models/user_display.rb
@@ -1,4 +1,10 @@
 class UserDisplay
   def initialize(parent:, story:)
+    @parent = parent
+    @story = story
+  end
+
+  def referrer_name
+    @parent.name
   end
 end
--- spec/app/models/user_display_spec.rb
+++ spec/app/models/user_display_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 describe UserDisplay do
+  describe 'referrer_name' do
+    let(:parent_user)  { double(:parent_user, name: 'parent_username') }
+    let(:user_display) { UserDisplay.new(parent: parent_user, story: 'a long story') }
+
+    it 'returns the users referral name' do
+      expect(user_display.referrer_name).to match(/parent_username/)
+    end
+  end
 end
