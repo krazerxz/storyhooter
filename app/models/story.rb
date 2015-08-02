5d4652b0969e53255c5d94ebbf86eebf12fe0e9d
Chris Pomfret <chris.pomfret@bt.com>
2015-08-02 20:14:51 +0100

    build_up and build_down in story

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,5 @@
 class Story
-  def self.build_from(user_uuid:)
+  def self.build_to_top_from(user_uuid:)
     current_user = User.find_by(user_uuid: user_uuid)
     story = []
     until current_user.parent.nil?
@@ -8,5 +8,23 @@ class Story
     end
     story << current_user.tale
     story.reverse
+  end
+
+  def self.build_down_from(user_uuid:)
+    current_user = User.find_by(user_uuid: user_uuid)
+    return [] if children_for(current_user).count.zero?
+    story = []
+    until children_for(current_user).count.zero?
+      current_user = children_for(current_user).sample
+      story << current_user.tale
+    end
+    story
+  end
+
+  private
+
+  def self.children_for user
+    user.children.execute
+    user.children
   end
 end
