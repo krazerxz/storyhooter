763ed3e16fb02066f7a258e8e13c9449efa51ddb
Chris Pomfret <chris.pomfret@bt.com>
2015-08-02 15:48:56 +0100

    Failing feature for story evolution

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,9 +2,20 @@ Given 'there is an existing completed user' do
   User.create!(name: 'primary_user', tale: 'Once upon a time', completed: true)
 end
 
+Given 'there is an existing story' do
+  user_1 = User.create!(name: 'user_1', tale: 'Part 1')
+  user_2 = User.create!(name: 'user_2', tale: 'Part 2', referrer_id: user_1.id)
+  User.create!(name: 'user_3', tale: 'Part 3', referrer_id: user_2.id)
+end
+
 When 'I visit the new user page with the existing user uuid' do
   user = User.all.first
   visit "/user/new?referred_from=#{user.uuid}"
+end
+
+When 'I visit a user in the middle of the chain' do
+  user = User.find_by(name: 'user_2')
+  visit "/user/#{user.id}"
 end
 
 Then 'I should see the new user page' do
@@ -24,4 +35,10 @@ Then 'I see the user show page' do
   expect(find('#story-so-far').text).to match(/Once upon a time/)
   expect(find('#story-so-far').text).to match(/The End/)
   expect(find('#referral-url')[:href]).to match(%r{/user/new\?referred_from=#{uuid}})
+end
+
+Then 'I should see the future of the story' do
+  expect(find('#story-so-far').text).to match(/Part 1/)
+  expect(find('#story-so-far').text).to match(/Part 2/)
+  expect(find('#story-evolution').text).to match(/Part 3/)
 end
