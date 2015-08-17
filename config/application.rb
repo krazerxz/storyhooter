2586eeab301cb977e6afc831bf505e4ccb7e64db
Chris Pomfret <chris.pomfret@bt.com>
2015-08-17 09:52:00 +0100

    rebrand: Rename to StoryHooter

--- config/application.rb
+++ config/application.rb
@@ -5,24 +5,10 @@ require 'action_mailer/railtie'
 require 'sprockets/railtie'
 require 'rails/test_unit/railtie'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module WordOfMouth
+module StoryHooter
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
     neo4j_dbconfig = YAML.load(File.open("#{File.dirname(__FILE__)}/neo4j_database.yml"))[ENV['RAILS_ENV']]
     Neo4j::Session.open(:server_db, neo4j_dbconfig['database_url'])
   end
