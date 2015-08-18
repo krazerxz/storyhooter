c33ca512849619043c4c499b886578ba9b8a0925
Chris Pomfret <chris.pomfret@bt.com>
2015-08-18 12:26:59 +0100

    Homepage uses user_display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   def index
     @users = User.all
     user = User.first
-    @story_so_far = Story.build_to_top_from(user_uuid: user.user_uuid)
+    story_so_far = Story.build_to_top_from(user_uuid: user.user_uuid)
+    @user_display = UserDisplay.new(story: story_so_far)
     render 'index'
   end
 end
