58433a56c1367f8c03db9de31ddf4cbe90aad0fc
Chris Pomfret <cpom40ba@gmail.com>
2017-01-02 23:00:48 +0000

    put a user in the seed file for reference

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 if User.count.zero?
-  User.create country: "UK", email: "example@example.com", name: "example", tale: "example_tale"
+  User.create country: "UK", email: "example@example.com", name: "example", tale: "example_tale", parent: nil, children: [], user_uuid: 1
 end
